bc4e27a642e3199a35827efcdca87c0b7730a057
mahmudahmod007 <90406323+mahmudahmod007@users.noreply.github.com>
2022-01-18 18:30:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, redirect, url_for, request, jsonify
+
+app = Flask(__name__)
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+import os
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id="4315723999734c089a601f25a082abcc",
+    client_secret="585dc2e2e08946e29d47899045a242fc"
+)
+)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        search_text = request.form['nm']
+
+        results = sp.search(q=search_text, limit=10)
+        # for idx, track in enumerate(results['tracks']['items']):
+        # 	print(idx, track['name'])
+        songlist = results['tracks']['items']
+
+        return render_template('spotify-flask.html', tracks=songlist)
+    # return jsonify(results)
+
+    else:
+        user = request.args.get('nm')
+        return render_template('spotify-flask.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
